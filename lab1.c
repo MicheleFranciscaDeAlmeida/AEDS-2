60b9b57d9ae95512b882655ff0c393e9d0ee9be0
Michele Francisca De Almeida <224978364+MicheleFranciscaDeAlmeida@users.noreply.github.com>
2026-08-11 13:22:45 +0000

    Programa executado

---
+++ lab1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int contChar(char str[]) {
+    int i;
+    for(i = 0; str[i] != '\0'; i++) {
+    }
+    return i;
+}
+
+int main() {
+    char pa[100];
+    scanf(" %[^\n]", pa); 
+    printf("%d", contChar(pa));
+
+    while(!(pa[0] == 'F' && pa[1] == 'I' && pa[2] == 'M' && pa[3] == '\0')) {
+        scanf(" %[^\n]", pa);
+        printf("%d", contChar(pa));
+
+    }
+    return 0; 
+}
