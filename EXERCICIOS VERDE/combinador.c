9d8f07f83d3252f6e9cd2bc2155a9ea4cffcdee5
Michele Francisca De Almeida <224978364+MicheleFranciscaDeAlmeida@users.noreply.github.com>
2026-08-24 19:56:20 +0000

    Organizando exercicios e configurando o gitignore

---
+++ EXERCICIOS VERDE/combinador.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s1[100], s2[100];
+
+    while(scanf("%s %s", s1, s2) != EOF) {
+        int l1 = strlen(s1);
+        int l2 = strlen(s2);
+        int maxl = l1 > l2 ? l1 : l2;
+        char resultado [200];
+        int ind = 0;
+
+    for (int i = 0; i < maxl; i++) {
+        if(i < l1) {
+            resultado [ind++] = s1[i];
+        }
+        if(i < l2) {
+            resultado [ind++] = s2[i];
+        }
+    }
+    resultado[ind] = '\0';
+    printf("%s\n", resultado);
+    }
+    return 0; 
+}
+
+/*
+Uso de IA: ChatGPT utilizado como ferramenta de apoio ao estudo,
+esclarecimento de dúvidas, pesquisa de documentação e depuração.
+A implementação e as decisões sobre o código foram realizadas pela aluna.
+*/
