68d488fe8a5edfd9a4c34c739a62a6a26e9c44e9
Michele Francisca De Almeida <224978364+MicheleFranciscaDeAlmeida@users.noreply.github.com>
2026-08-25 22:35:58 +0000

    Adiciona TP 1 CiframentoCesar.java

---
+++ TPS/TP 1/CiframentoCesar/CiframentoCesar.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class CiframentoCesar {
+
+    // Recebe uma String e retorna a mesma String com as letras deslocadas 3 posições.
+    public static String cifrar(String texto) {
+    String resultado = "";
+    
+    // Percorre todos os caracteres da String.
+    for (int i = 0; i < texto.length(); i++) {
+
+        // Armazena em c o caractere que está sendo analisado.
+        char c = texto.charAt(i);
+    
+    // Verifica se o caractere é uma letra minúscula.
+    if (c >= 'a' && c <= 'z') {
+
+        // Desloca a letra 3 posições, voltando ao início após o 'z'.
+        c = (char) ('a' + (c - 'a' + 3) % 26);
+    
+    // Verifica se o caractere é uma letra maiúscula.
+    } else if (c >= 'A' && c <= 'Z') {
+        
+        //Desloca a letra 3 posicões, voltando ao início após o 'Z'.
+        c = (char) ('A' + (c - 'A' + 3) % 26);
+    }
+
+    // Adiciona o caractere processado ao resultado
+    resultado += c;
+
+    }
+
+    return resultado;
+
+    }
+    // Faz a leitura das linhas da entrada, chama o método de ciframento e imprime cada resultado.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        // Lê e processa todas as linhas até o fim da entrada.
+        while (scanner.hasNextLine()) {
+            String texto = scanner.nextLine();
+        
+            System.out.println(cifrar(texto));
+        }
+        scanner.close();
+    }
+}
