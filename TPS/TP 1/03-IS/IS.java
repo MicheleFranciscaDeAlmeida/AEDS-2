1dc666189b29665d755d12c223153472d5d50026
Michele Francisca De Almeida <224978364+MicheleFranciscaDeAlmeida@users.noreply.github.com>
2026-08-27 14:15:30 +0000

    Início da implementação da questão IS

---
+++ TPS/TP 1/03-IS/IS.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class IS {
+    public static boolean Vogais(String s) {
+    boolean resultado = "";
+    // Percorre 
+    for (int i = 0; i < s.length(); i++) {
+        char c = s.charAt(i);
+
+        if (c == 'a' || c == 'e' || 'i' || 'o' || 'u') {
+            
+        }
+    }
+    }
+}
