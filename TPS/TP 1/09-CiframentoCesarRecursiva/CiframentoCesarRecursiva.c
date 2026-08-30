5e0973e5ab1fb05668e7e43451461eeeb27b3cbb
Michele Francisca De Almeida <224978364+MicheleFranciscaDeAlmeida@users.noreply.github.com>
2026-08-30 18:50:20 +0000

    Inclusão comentários

--- TPS/TP 1/09-CiframentoCesarRecursiva/CiframentoCesarRecursiva.c
+++ TPS/TP 1/09-CiframentoCesarRecursiva/CiframentoCesarRecursiva.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Método recursivo que percorre a string e aplica
+o deslocamento de 3 posições em cada caractere. */
 void CiframentoCesarRecursiva(char *s, int i)
 {
     if (s[i] != '\0')
@@ -14,7 +16,8 @@ void CiframentoCesarRecursiva(char *s, int i)
 int main()
 {
     char s[1000];
-    // Lê a linha inteira incluindo espaços até encontrar o fim do arquivo
+
+    // Faz a leitura da linha inteira incluindo espaços até encontrar o fim do arquivo
     while (fgets(s, sizeof(s), stdin) != NULL)
     {
 
@@ -29,6 +32,7 @@ int main()
             s[len - 1] = '\0';
         }
 
+        // Verifica se a entrada é "FIM".
         if (s[0] == 'F' && s[1] == 'I' &&
             s[2] == 'M' && s[3] == '\0')
         {
@@ -41,3 +45,14 @@ int main()
 
     return 0;
 }
+
+/*
+ * Uso de IA:
+ * A ferramenta de Inteligência Artificial foi utilizada como apoio
+ * para fundamentação, documentação e compreensão do enunciado,
+ * auxiliando na análise da lógica e na revisão do código.
+ *
+ * A implementação foi desenvolvida a partir do meu próprio
+ * raciocínio e entendimento do problema, com testes e validação
+ * realizados por mim.
+ */
